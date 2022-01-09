7a933bb2c144ad494d2e373c422fa3b329553a32
KyungNam Ko <rhrudska987@naver.com>
2022-01-09 17:12:29 +0900

    Solve problem

--- src/prac1/Problem.java
+++ src/prac1/Problem.java
@@ -1,35 +1,53 @@
 package prac1;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 class Solution {
-	public int[] solution(int[] array, int[][] commands) {		
-		List<Integer> answerList = new ArrayList<>();
-		
-		for(int i=0; i<commands.length; i++) {
-			List<Integer> arrList = new ArrayList<>();
-			for(int j=0; j<array.length; j++) {
-				arrList.add(array[j]);
-			}
-			List<Integer> list = arrList.subList(commands[i][0]-1, commands[i][1]);
-			Collections.sort(list);
-			answerList.add(list.get(commands[i][2]-1));	
-		}
-			
-        int[] answer = answerList.stream().mapToInt(i->i).toArray();
+    public int[] solution(int[] answers) {
+        int[] supo1 = {1,2,3,4,5};
+        int[] supo2 = {2,1,2,3,2,4,2,5};
+        int[] supo3 = {3,3,1,1,2,2,4,4,5,5};
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(0);
+        list.add(0);
+        
+        for(int i = 0; i<answers.length; i++) {
+        	if(answers[i] == supo1[i%5]) list.set(0, list.get(0)+1);
+        	if(answers[i] == supo2[i%8]) list.set(1, list.get(1)+1);
+        	if(answers[i] == supo3[i%10]) list.set(2, list.get(2)+1);
+        }
+        
+    	int max = 0;
+        for(int i=0; i<list.size(); i++) {
+        	if(max<list.get(i)) max = list.get(i);
+        }
+        
+    	ArrayList<Integer> listAnswer = new ArrayList<>();
+    	for(int i=0; i<list.size();i++) {
+    		if(max == list.get(i)) listAnswer.add(i+1);
+    	}
+    	
+    	int[] answer = new int[listAnswer.size()];
+    	
+    	for(int i=0; i<answer.length; i++) {
+    		answer[i] = listAnswer.get(i).intValue();
+    	}
+    	for(int i=0; i<answer.length; i++) {
+    		System.out.println(answer[i]);
+    	}
         return answer;
+        
     }
 }
 
 public class Problem {
 	 public static void main(String[] args) {
-		 int[] arr = {1,5,2,6,3,7,4};
-		 int [][] commands = {{2,5,3}, {4,4,1},{1,7,3}};
-		 Solution so = new Solution();
-		 System.out.println(so.solution(arr, commands));
-		
+		int[] answer = {5,4,4,2,1};
+		Solution so = new Solution();
+		so.solution(answer);
+		//System.out.println(so.solution(answer).toString());
 		 
 	 }
 }
--- src/prac2/Problem2.java
+++ src/prac2/Problem2.java
@@ -3,41 +3,54 @@ package prac2;
 import java.util.ArrayList;
 
 class Solution {
-	public int solution(int[] nums) {
-        int answer = 0;
-        ArrayList<Integer> list = new ArrayList<>();
-        ArrayList<Integer[]> list2 = new ArrayList<>();
-        for(int i=0; i<nums.length-2; i++) {//3개의 숫자 리스트에 집어넣기
-        	for(int j=i+1; j<nums.length-1; j++) {
-        		for(int k=j+1; k<nums.length; k++) {
-        			list2.add(new Integer[] {nums[i], nums[j], nums[k]});
-        		}
-        	}
-        }
-        
-        for(int i=0; i<list2.size(); i++) {
-        	int sum = list2.get(i)[0] + list2.get(i)[1] + list2.get(i)[2];    
-        	boolean isPrime = true;
-        	for(int j = 2; j<=Math.sqrt(sum); j++) {//소수판별
-        		if(sum%j==0) {
-        			isPrime = false;
-        			break;
-        		}
-        	}
-       		if(isPrime) {
-       			answer++;
-       			System.out.println("[" + list2.get(i)[0] + "," + list2.get(i)[1] + "," + list2.get(i)[2]
-       					+"]를 이용해서 " + sum + "을 만들 수 있습니다.");
-       		}
-        }
+    public int solution(int n, int[] lost, int[] reserve) { 	
+    	ArrayList<Integer> gymUniformList = new ArrayList<>();
+    	
+    	for(int i=0; i<n; i++) { //각 학생의 체육복 개수 체크 
+    		gymUniformList.add(1);
+    		
+    		for(int j=0; j<lost.length; j++) {//체육복도난 여부 체크
+    			if((i+1)==lost[j]) {
+    				gymUniformList.set(i, 0);
+    				break;
+    			}
+    		}
+    		
+    		for(int k=0; k<reserve.length; k++) {//여벌체육복 여부 체크
+    			if((i+1)==reserve[k]) {
+    				gymUniformList.set(i, gymUniformList.get(i)+1);
+    				break;
+    			}
+    		}
+    	}
+    	
+    	for(int i=0; i<gymUniformList.size()-1; i++) {
+    		if(gymUniformList.get(i)==2&&gymUniformList.get(i+1)==0) {
+    			gymUniformList.set(i, 1);
+    			gymUniformList.set(i+1, 1);
+    		}
+    		else if(gymUniformList.get(i)==0&&gymUniformList.get(i+1)==2) {
+    			gymUniformList.set(i, 1);
+    			gymUniformList.set(i+1, 1);
+    		}
+    		
+    	}
+    	
+    	int answer = 0;
+    	for(int i=0; i<gymUniformList.size(); i++) {
+    		if(gymUniformList.get(i)!=0) answer++;
+    	}
+    	
         return answer;
     }
 }
-
 public class Problem2 {
 	 public static void main(String[] args) {
-	    	int num[] = {1,2,7, 6, 4};
-	    	Solution so = new Solution();
-	    	so.solution(num);
-	    }
+		 int n = 10;
+		 int[] lost = {3,9,10};
+		 int[] reserve = {3,8,9};
+		 
+		 Solution so = new Solution();
+		 System.out.println(so.solution(n, lost, reserve));
+	 }
 }
